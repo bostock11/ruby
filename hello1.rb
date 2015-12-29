c534e829f73b5fa11eca3e211712c05605aea993
outreach11 <anthonygbostock@gmail.com>
2015-12-28 16:38:34 -0800

    worked to page 18 in book

---
+++ hello1.rb
@@ -0,0 +1,9 @@
+def say_goodnight(name)
+	result = "Good night, #{name}"
+	return result
+end
+
+puts say_goodnight("anthony")
+puts say_goodnight("chad")
+
+
---
+++ myprog.rb
@@ -0,0 +1,8 @@
+puts "hello"
+puts "it is now #{Time.now}"
+
+
+# shows absolut value of num. abs is built into variables
+num = -1234
+absolute_value = num.abs
+puts num.abs
